ebd775fa4828c22b703fb1258c90ddaaf2b11f53
Tikhon <tikhonshamruk@gmail.com>
2025-09-29 21:47:41 +0300

    making middleware with arcjet

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,34 @@
+import  arcjet, {detectBot, shield, tokenBucket}  from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { request } from 'http';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', "/"])
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!, // твой ключ из .env.local
+  rules: [
+    shield({mode: "LIVE"}),
+    detectBot({
+      mode: "LIVE",
+      allow:[]
+    }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5, // не больше 5 запросов в минуту
+      interval:10,
+      capacity: 5,
+    }),
+  ],
+});
+
 export default clerkMiddleware(async (auth, req) => {
+
+  const decision = await aj.protect(req,{requested: 5})
+
+  if(decision.isDenied()){
+    return new Response(null, {status: 403})
+  }
+
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
