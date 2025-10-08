38c1ecc6d5a598a638bb2c9824edcdd8af56d7e9
Tikhon <tikhonshamruk@gmail.com>
2025-10-08 10:24:03 +0300

    setting up prettier and doing keeping developing my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { SignInButton, UserButton } from '@clerk/nextjs'
 
-export default function HomePage(){
-  return <>
-  <SignInButton />
-  <UserButton />
-  <ModeToggle />
-  </>
+import { ModeToggle } from '@/components/mode-toggle'
+
+export default function HomePage() {
+	return (
+		<>
+			<SignInButton />
+			<UserButton />
+			<ModeToggle />
+		</>
+	)
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,44 @@
-import  arcjet, {detectBot, shield, tokenBucket}  from '@arcjet/next';
+import { env } from './data/env'
+import arcjet, { detectBot, shield, tokenBucket } from '@arcjet/next'
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { request } from 'http';
-import { env } from './data/env';
+import { request } from 'http'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', "/"])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/'])
 
 const aj = arcjet({
-  key: env.ARCJET_KEY, // твой ключ из .env.local
-  rules: [
-    shield({mode: "LIVE"}),
-    detectBot({
-      mode: "LIVE",
-      allow:[]
-    }),
-    tokenBucket({
-      mode: "LIVE",
-      refillRate: 5, // не больше 5 запросов в минуту
-      interval:10,
-      capacity: 5,
-    }),
-  ],
-});
+	key: env.ARCJET_KEY, // твой ключ из .env.local
+	rules: [
+		shield({ mode: 'LIVE' }),
+		detectBot({
+			mode: 'LIVE',
+			allow: []
+		}),
+		tokenBucket({
+			mode: 'LIVE',
+			refillRate: 5, // не больше 5 запросов в минуту
+			interval: 10,
+			capacity: 5
+		})
+	]
+})
 
 export default clerkMiddleware(async (auth, req) => {
+	const decision = await aj.protect(req, { requested: 1 })
 
-  const decision = await aj.protect(req,{requested: 1})
+	if (decision.isDenied()) {
+		return new Response(null, { status: 403 })
+	}
 
-  if(decision.isDenied()){
-    return new Response(null, {status: 403})
-  }
-
-  if (!isPublicRoute(req)) {
-    await auth.protect()
-  }
+	if (!isPublicRoute(req)) {
+		await auth.protect()
+	}
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+		// Always run for API routes
+		'/(api|trpc)(.*)'
+	]
 }
